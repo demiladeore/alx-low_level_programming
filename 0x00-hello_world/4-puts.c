8f2760a49f9f9eda5a78d7ea2dd872ef3ba28143
demiladeore <oluwafemidemi47@gmail.com>
2022-09-11 14:36:37 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/**
+* main - functioin that prints a string
+* Description - Prints a string using puts not printf
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n")
+	return (0);
+}
