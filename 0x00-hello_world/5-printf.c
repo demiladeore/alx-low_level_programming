ab6a30c0d6981d9e96f8de05e2dbe158c24ab600
demiladeore <oluwafemidemi47@gmail.com>
2022-09-11 15:03:18 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - function prints a string
+* Description - prints a string with printf
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("ith proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
