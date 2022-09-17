4b9f5bb5b33503b41c8972ae8fe68fd2c5edb5ec
demiladeore <oluwafemidemi47@gmail.com>
2022-09-16 17:24:10 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* mul - Multiply two inegers
+* @a: first func argument
+* @b: secong function argument 
+* Return: Always 0
+*/
+
+int mul(int a, int b)
+{
+	{
+		return (a * b);
+	}
+}
