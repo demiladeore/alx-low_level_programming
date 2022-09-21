8909850a518b4165c5a7644563cac786680a2957
demiladeore <oluwafemidemi47@gmail.com>
2022-09-21 14:16:17 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - write a function that resets to 98
+ * @n: integer pointer to be updated
+ * Return: always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
