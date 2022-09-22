91f9f9a79696f983f46c45b067173f99f1a8a3e8
demiladeore <oluwafemidemi47@gmail.com>
2022-09-22 15:51:55 -0700

    Task 2 - 5

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - function that copies a string
+ * @dest: string parameter1
+ * @src: string parameter2
+ * @n: parameter3
+ * Return: string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - function that compares two strings
+ * @s1: string parameter1
+ * @s2: string parameter2
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int solution;
+
+	solution = strcmp(s1, s2);
+	return (solution);
+}
